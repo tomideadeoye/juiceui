1f949923594cfaadcb267b707f2d598cb2db402b
Tomide Adeoye <tommideadeoye@gmail.com>
2023-01-06 21:30:07 +0100

    4 framer-motion update

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,178 +1,119 @@
+import { Chip, Stack, TextField, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React from "react";
+import { AnimationMakerCards } from "../components/animations";
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles((theme) => ({
+	container: {
+		padding: theme.spacing(2, 5),
+		[theme.breakpoints.down("md")]: {
+			padding: theme.spacing(2, 3),
+		},
+	},
+
+	cardBox: {
+		borderRadius: theme.borderRadius.md,
+		backgroundColor: theme.palette.white.main,
+		// padding: theme.spacing(2, 2),
+
+		"& > img": {
+			borderRadius: theme.borderRadius.md,
+		},
+
+		"&:hover": {
+			boxShadow: theme.shadows[2],
+			padding: theme.spacing(0, 0),
+		},
+	},
+}));
+
+const articles = [
+	{
+		id: 1,
+		title: "A Short Survey of the business models of DeFi platforms",
+	},
+	{
+		id: 2,
+		title: "Taming the Wild West of DeFi: A Guide to DeFi Risk Management",
+	},
+	{ id: 3, title: "Binance Smart Chain: A New Frontier for DeFi" },
+];
 
 const Articles = () => {
 	const styles = useStyles();
 
 	return (
-		<div className={styles.articles}>
-			<img className={styles.forwardIcon} alt="" src="../forward@2x.png" />
-			<div className={styles.div}>
-				<div className={styles.makingAGeneralInquiryOrY}>
-					Making a general inquiry, or you’ll like us to talk in detail? Tell us
-					what you need and we will get back to you as soon as possible.
-				</div>
-				<b className={styles.articles1}>Articles</b>
-			</div>
-			<div className={styles.topBgIcons}>
-				<img
-					className={styles.juice3dIllustrationSecurityIcon}
-					alt=""
-					src="../juice3d-illustrationsecurity060922eo@2x.png"
-				/>
-				<img
-					className={styles.juiceIconsBankTransfer24x2}
-					alt=""
-					src="../juiceiconsbanktransfer24x24-1@2x.png"
-				/>
-				<img
-					className={styles.juiceIconsBankTransfer24x21}
-					alt=""
-					src="../juiceiconsbanktransfer24x24-1@2x.png"
-				/>
-				<img
-					className={styles.juiceIconsPaymentTriggered}
-					alt=""
-					src="../juiceiconspaymenttriggered88x88-112@2x.png"
-				/>
-				<img
-					className={styles.juiceIconsBankTransfer32x3}
-					alt=""
-					src="../juiceiconsbanktransfer32x32-2@2x.png"
-				/>
-				<img
-					className={styles.juice3dIllustrationExchangeIcon}
-					alt=""
-					src="../juice3d-illustrationexchangeratev3-2@2x.png"
-				/>
-				<img
-					className={styles.juice3dIllustrationExchangeIcon1}
-					alt=""
-					src="../juice3d-illustrationexchangeratev3-3@2x.png"
-				/>
-				<div className={styles.icons88progress}>
-					<img
-						className={styles.juiceIconsTransactionInPro}
-						alt=""
-						src="../juiceiconstransactioninprogress88x889@2x.png"
-					/>
-				</div>
-				<div className={styles.icons88progress1}>
-					<img
-						className={styles.juiceIconsTransactionInPro}
-						alt=""
-						src="../juiceiconstransactioninprogress88x8810@2x.png"
-					/>
-				</div>
-				<div className={styles.icons88progress2}>
-					<img
-						className={styles.juiceIconsTransactionInPro}
-						alt=""
-						src="../juiceiconstransactioninprogress88x8810@2x.png"
-					/>
-				</div>
-				<img
-					className={styles.juice3dIllustrationCvv060Icon}
-					alt=""
-					src="../juice3d-illustrationcvv060922eo@2x.png"
-				/>
-				<img
-					className={styles.juice3dIllustrationCvv060Icon1}
-					alt=""
-					src="../juice3d-illustrationcvv060922eo1@2x.png"
-				/>
-			</div>
-			<img className={styles.imgIcon} alt="" src="../img20@2x.png" />
-			<div className={styles.articlesInner}>
-				<div className={styles.groupParent}>
-					<div className={styles.rectangleParent}>
-						<div className={styles.groupChild} />
-						<div className={styles.category}>Category</div>
-					</div>
-					<div className={styles.rectangleGroup}>
-						<div className={styles.groupChild} />
-						<div className={styles.category}>Category</div>
-					</div>
-					<div className={styles.rectangleContainer}>
-						<div className={styles.groupChild} />
-						<div className={styles.category}>Category</div>
-					</div>
-					<div className={styles.groupDiv}>
-						<div className={styles.groupChild} />
-						<div className={styles.category}>Category</div>
-					</div>
-					<div className={styles.rectangleParent1}>
-						<div className={styles.groupChild} />
-						<div className={styles.category}>Category</div>
-					</div>
-					<div className={styles.rectangleParent2}>
-						<div className={styles.groupChild} />
-						<div className={styles.category}>Category</div>
-					</div>
-					<b className={styles.discoverMoreOfWhatMatters}>
-						Discover more of what matters to you
-					</b>
-				</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-			<div className={styles.image2Parent}>
-				<img className={styles.image2Icon} alt="" src="../image-2@2x.png" />
-				<b className={styles.articleTitleGoesHereAndIt}>
-					Article title goes here and it should be long up to two lines
-				</b>
-				<div className={styles.categoryCategory}>Category • Category</div>
-			</div>
-		</div>
+		<Stack className={styles.container} spacing={8} alignItems="center" mb={10}>
+			<Typography variant="h3" fontWeight={700} textAlign="center" mt={5}>
+				Articles
+			</Typography>
+
+			<Typography
+				variant="h6"
+				fontWeight={300}
+				textAlign="center"
+				maxWidth={800}
+			>
+				Making a general inquiry, or you’ll like us to talk in detail? Tell us
+				what you need and we will get back to you as soon as possible.
+			</Typography>
+			<Stack direction="row" spacing={3}>
+				<Chip label="Payments" color="primary" />
+				<Chip label="Fintech" color="primary" />
+				<Chip label="Africa" color="primary" />
+				<Chip label="Stable Coins" color="primary" />
+			</Stack>
+
+			<TextField
+				id="standard-basic"
+				label="Search a post"
+				variant="standard"
+				fullWidth
+				sx={{ m: 1 }}
+			/>
+
+			<Stack spacing={6} m={4}>
+				{articles.map((item) => {
+					return (
+						<AnimationMakerCards key={item.id}>
+							<Stack
+								direction={{ sm: "column", md: "row" }}
+								justifyContent="space-between"
+								spacing={3}
+								alignItems="center"
+								className={styles.cardBox}
+							>
+								<Stack
+									spacing={3}
+									sx={{
+										padding: { sm: "1rem", md: "2rem" },
+									}}
+								>
+									<Typography
+										variant="body1"
+										fontWeight={600}
+										gutterBottom
+										maxWidth={300}
+									>
+										{item.title}
+									</Typography>
+									<Stack direction="row" spacing={3}>
+										<Chip label="Category" color="primary" />
+										<Chip label="Category" variant="outlined" color="warning" />
+									</Stack>
+								</Stack>
+								<img
+									className={styles.imgIcon}
+									alt=""
+									src="../img20@2x.png"
+									height={300}
+								/>
+							</Stack>
+						</AnimationMakerCards>
+					);
+				})}
+			</Stack>
+		</Stack>
 	);
 };
 
