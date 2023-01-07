4335203060289278cd0d410f7277e9afb031d2eb
Tomide Adeoye <tommideadeoye@gmail.com>
2023-01-07 03:20:39 +0100

    wrapping up

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -20,6 +20,16 @@ const useStyles = makeStyles(() => ({
 		maxWidth: 500,
 		padding: theme.spacing(2, 2),
 	},
+
+	imageBrand1: {
+		width: "100%",
+	},
+	imageBrand2: {
+		width: 200,
+		[theme.breakpoints.down("md")]: {
+			width: 120,
+		},
+	},
 }));
 
 const handleSubmit = (e) => {
@@ -39,7 +49,7 @@ const ContactUs = () => {
 					mb={5}
 					spacing={3}
 				>
-					<Stack spacing={3}>
+					<Stack spacing={3} mb={5}>
 						<Typography variant="h3" fontWeight={700} gutterBottom>
 							How can we help?
 						</Typography>
@@ -53,96 +63,90 @@ const ContactUs = () => {
 							Tell us what you need and we will get back to you as soon as
 							possible.
 						</Typography>
-						<img
-							className={styles.credentialsChild}
+						<Box
+							component="img"
+							className={styles.imageBrand1}
 							alt=""
 							src="../group-821.svg"
-							width={400}
 						/>
-						<img
-							className={styles.credentialsItem}
-							alt=""
-							src="../group-83.svg"
-							width={160}
-						/>
+
 						<Typography fontSize="10px" fontWeight={100} letterSpacing=".5px">
 							Diverse industry leaders use Juice
 						</Typography>
 					</Stack>
-					<Stack>
-						<Stack className={styles.form}>
-							<Box component="form" onSubmit={handleSubmit} noValidate>
-								<TextField
-									margin="normal"
-									required
-									fullWidth
-									id="email"
-									label="First name (required)"
-									name="email"
-									autoComplete="email"
-									autoFocus
-									borderRadius={theme.borderRadius.md}
-								/>
-								<TextField
-									margin="normal"
-									required
-									fullWidth
-									id="email"
-									label="Last name (required)"
-									name="email"
-									autoComplete="email"
-									autoFocus
-								/>{" "}
-								<TextField
-									margin="normal"
-									required
-									fullWidth
-									id="email"
-									label="Company email (required)"
-									name="email"
-									autoComplete="email"
-									autoFocus
-								/>
-								<TextField
-									margin="normal"
-									required
-									fullWidth
-									id="email"
-									label="Company (required)"
-									name="email"
-									autoComplete="email"
-									autoFocus
-								/>{" "}
-								<TextField
-									margin="normal"
-									required
-									fullWidth
-									id="email"
-									label="Message (required)"
-									name="email"
-									autoComplete="email"
-									autoFocus
-									multiline
-									rows={5}
-								/>
-								<Button
-									type="submit"
-									fullWidth
-									variant="contained"
-									sx={{
-										backgroundColor: theme.palette.grey[800],
-										mt: 3,
-										mb: 2,
-										padding: "16px 0",
-										"&:hover": {
-											backgroundColor: theme.palette.primary.dark,
-										},
-									}}
-								>
-									Sign In
-								</Button>
-							</Box>
-						</Stack>
+
+					<Stack className={styles.form}>
+						<Box component="form" onSubmit={handleSubmit} noValidate>
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="First name (required)"
+								name="email"
+								autoComplete="email"
+								autoFocus
+								borderRadius={theme.borderRadius.md}
+							/>
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Last name (required)"
+								name="email"
+								autoComplete="email"
+								autoFocus
+							/>{" "}
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Company email (required)"
+								name="email"
+								autoComplete="email"
+								autoFocus
+							/>
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Company (required)"
+								name="email"
+								autoComplete="email"
+								autoFocus
+							/>{" "}
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Message (required)"
+								name="email"
+								autoComplete="email"
+								autoFocus
+								multiline
+								rows={5}
+							/>
+							<Button
+								type="submit"
+								fullWidth
+								variant="contained"
+								sx={{
+									backgroundColor: theme.palette.grey[800],
+									mt: 3,
+									mb: 2,
+									padding: "16px 0",
+									"&:hover": {
+										backgroundColor: theme.palette.primary.dark,
+									},
+								}}
+							>
+								Sign In
+							</Button>
+						</Box>
 					</Stack>
 				</Stack>
 
